53313f6f99661e21d49be3b73d3e144bcf3bcae0
Tao He <1051788114@qq.com>
2024-03-18 16:14:09 +0800

    增加循环写法

--- 二分查找.py
+++ 二分查找.py
@@ -1,12 +1,25 @@
 # 二分查找模版
+# 递归写法
 def binarySearch (arr, l, r, x): 
-            mid = l + (r-l)//2
-            if r>=l:
-                if arr[mid]==x:
-                    return mid
-                elif arr[mid]>x:
-                    return binarySearch(arr, l, mid-1, x)
-                elif arr[mid]<x:
-                    return binarySearch(arr, mid+1, r, x)
-            else:
-                return mid+1
+    mid = l + (r-l)//2
+    if r>=l:
+        if arr[mid]==x:
+            return mid
+        elif arr[mid]>x:
+            return binarySearch(arr, l, mid-1, x)
+        elif arr[mid]<x:
+            return binarySearch(arr, mid+1, r, x)
+    else:
+        return mid+1
+    
+# 循环写法 （求算数平方根）
+def binarySearch (l, r, x): 
+    l, r, ans = 0, x, -1
+    while l <= r:
+        mid = (l + r) // 2
+        if mid * mid <= x:
+            ans = mid
+            l = mid + 1
+        else:
+            r = mid - 1
+    return ans
